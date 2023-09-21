50c6581ea3721ff71218864a79739eb93e0875da
milcho0604 <milcho0604@gmail.com>
2023-09-21 14:51:59 +0900

    런타임에러(NoSuchElement) 발생 후 수정

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,13 @@ public class Main {
                 arr[i][j] = sc.nextInt();   // 9*9배열을 사용자로부터 입력받음.
                 if(max < arr[i][j]){    // max의 값이 arr[i][j]보다 작으면
                     max = arr[i][j];    // max의 값을 arr[i][j]로 치환.
-                    x = i+1;    // 위치의 경우 배열은 0부터 시작하므로 1부터 시작할 수 있도록 +1함
-                    y = j+1;    // 1~9 배열을 만들기 위한 단순 시각 편리화.
+                    x = i;    // 위치의 경우 배열은 0부터 시작하므로 1부터 시작할 수 있도록 +1함
+                    y = j;    // 1~9 배열을 만들기 위한 단순 시각 편리화.
                 }
             }
         }
         System.out.println(max);    // max의 값 출력.
-        System.out.print(x+ " " +y);    // max의 위치(행렬)를 출력.
+        System.out.print((x+1)+ " " +(y+1));    // max의 위치(행렬)를 출력.
     }
 
 }
